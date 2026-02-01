9e8331b3a1d77aa272874cee3c03a8f72bb7bf4f
Ahmed-Antigravity-99 <ahmed@husin.org>
2026-02-01 03:16:27 +0300

    Update page.js

--- app/products/page.js
+++ app/products/page.js
@@ -1,6 +1,10 @@
 import ProductTable from "@/components/ProductTable";
 
-export default function ProductsPage() {
+// We make the function 'async' to match Next.js 15 standards
+export default async function ProductsPage({ searchParams }) {
+  // We unwrap searchParams in case you add filtering/searching later
+  const params = await searchParams;
+
   return (
     <div className="p-6">
       <ProductTable />
